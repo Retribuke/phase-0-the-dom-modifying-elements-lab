93775f93a49abd550ea6e60c6c6dfd1429ba42a7
Justyn Enyart <gweeners17@gmail.com>
2022-06-29 06:39:54 -0400

    commit tree 6903b0aa8fe6cda0d90c2fa0b4bef34d9475ff6a

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+function mainRemove() {
+    const main = document.getElementById('main');
+    main.remove();
+}
+
+mainRemove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Justyn is the champion';
+document.body.append(newHeader);
